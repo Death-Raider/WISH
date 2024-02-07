c513f763404e39cb4136bd1af4589e472b11fa4c
Death-Raider <oragimirox@gmail.com>
2024-02-08 02:59:52 +0530

    todo list created

--- api/server.py
+++ api/server.py
@@ -1,3 +1,12 @@
 """
-uses core files to create higher level functions for processing
+- Uses core files to create higher level functions for processing which include:
+* Obtaining information using OCR and update user profile
+* Giving proper inputs for research papers and maintaining fixed results 
+* Filter chats for explict content and words
+
+- Handles the database queries which include:
+* storing and retreving user profile details in the format (./data/user_data_template.json)
+* store and retreving text message for chatrooms
 """
+
+#connect to database
